55abf8b07ab2d388b965c41fd1771c74626d6175
Josia Almeida <josiaalmeida0001@gmail.com>
2021-10-31 21:57:29 +0000

    create map using leaflet

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -7,14 +7,21 @@ function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
-        <title>React Avançado - Boilerplate</title>
+        <title>My Trips</title>
         <link rel="shortcut icon" href="/img/icon-512.png" />
         <link rel="apple-touch-icon" href="/img/icon-512.png" />
         <link rel="manifest" href="/manifest.json" />
+        {/*Adicionando estilizacao do leaflet*/}
+        <link
+          rel="stylesheet"
+          href="https://unpkg.com/leaflet@1.7.1/dist/leaflet.css"
+          integrity="sha512-xodZBNTC5n17Xt2atTPuE1HxjVMSvLVW9ocqUKLsCC5CXdbqCmblAshOMAS6/keqq/sMZMZ19scR4PsZChSR7A=="
+          crossOrigin=""
+        />
         <meta name="theme-color" content="#06092B" />
         <meta
           name="description"
-          content="A simple project starter to work with TypeScript, React, NextJS and Styled Components"
+          content="A simple project to show my favorite spots in the word."
         />
       </Head>
       <GlobalStyles />
